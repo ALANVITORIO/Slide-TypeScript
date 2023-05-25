c0f8a6211a6a623fd15e4bfe866570f8db3c1d0b
Alan Vitório <alanvitorio1985@gmail.com>
2023-05-24 22:21:32 -0300

    ajustes-finais

--- src/Timeout.ts
+++ src/Timeout.ts
@@ -3,23 +3,24 @@ export default class Timeout {
   handler;
   start;
   timeLeft;
-  constructor(handler: TimerHandler, time: number) {
-    this.id = setTimeout(handler, time);
-    this.handler = handler;
-    this.start = Date.now();
-    this.timeLeft = time;
+  constructor(handler, time) {
+      this.id = setTimeout(handler, time);
+      this.handler = handler;
+      this.start = Date.now();
+      this.timeLeft = time;
   }
   clear() {
-    clearTimeout(this.id);
+      clearTimeout(this.id);
   }
   pause() {
-    const passed = Date.now() - this.start;
-    this.timeLeft = this.timeLeft - passed;
-    this.clear();
+      const passed = Date.now() - this.start;
+      this.timeLeft = this.timeLeft - passed;
+      this.clear();
   }
   continue() {
-    this.clear();
-    this.id = setTimeout(this.handler, this.timeLeft);
-    this.start = Date.now();
+      this.clear();
+      this.id = setTimeout(this.handler, this.timeLeft);
+      this.start = Date.now();
   }
 }
+//# sourceMappingURL=Timeout.js.map
